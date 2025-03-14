9cf53b80b319e3a7696d06931c105aa6f8270a71
gus <rosalesagustavo@gmail.com>
2025-03-14 15:15:58 -0400

    exit builtin

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,12 @@ fn main() {
         let stdin = io::stdin();
         let mut input = String::new();
         stdin.read_line(&mut input).unwrap();
+
+        let args: Vec<&str> = input.split(" ").collect();
+        if args[0].trim() == "exit" && args[1].trim() == "0" {
+            break;
+        }
+
         println!("{}: command not found", input.trim());
     }
 }
