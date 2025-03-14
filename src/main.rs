ace55def63151a1587a43d9ac17b028408e24912
gus <rosalesagustavo@gmail.com>
2025-03-14 14:45:49 -0400

    REPL fixed

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,10 @@
 use std::io::{self, Write};
 
 fn main() {
-    print!("$ ");
-    io::stdout().flush().unwrap();
     
     loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
         // Wait for user input
         let stdin = io::stdin();
         let mut input = String::new();
