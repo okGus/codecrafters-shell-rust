cebc6832c027f6c9924a1e031f1ee4304b1d0419
gus <rosalesagustavo@gmail.com>
2025-03-26 17:51:58 -0400

    refactor and program

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, path::Path};
+use std::{collections::HashMap, path::Path, process::Command};
 use once_cell::sync::Lazy;
 use std::env;
 
@@ -13,65 +13,63 @@ static BUILTIN: Lazy<HashMap<&'static str, &'static str>> = Lazy::new(|| {
     ])
 });
 
+fn handle_type_command(args: &[&str]) {
+    let cmd = args[0];
+    if let Some(builtin_desc) = BUILTIN.get(cmd) {
+        println!("{}", builtin_desc);
+        return;
+    }
+
+    if let Some(path) = env::var_os("PATH") {
+        for dir in env::split_paths(&path) {
+            let full_path = dir.join(cmd);
+            if full_path.is_file() {
+                println!("{} is {}", cmd, full_path.display());
+                return;
+            }
+        }
+    }
+
+    println!("{}: not found", cmd);
+}
+
 fn parse(input: String) {
-    let path = if let Ok(p) = env::var("PATH") {
-        p
-    } else {
-        String::new()
-    };
+    let args: Vec<&str> = input.split_whitespace().collect();
 
-    let args: Vec<&str> = input.split(" ").collect();
-
-    if args.len() > 1 {
-        // Builtin `exit`
-        if args[0].trim() == "exit" && args[1].trim() == "0" {
-            std::process::exit(0)
-        }
-
-        // Builtin `echo`
-        if args[0].trim() == "echo" {
-            print!("{}", args[1..].join(" "));
-            return;
-        }
-
-        // Builtin `type` builtins
-        if args[0].trim() == "type" {
-            let cmd: &str = args[1].trim();
-            if BUILTIN.contains_key(cmd) {
-                println!("{}", BUILTIN[cmd]);
-            }
-            // if PATH is provided, make fullpath and see if it exists
-            else if !path.is_empty() {
-                let list_paths: Vec<&str> = path.split(":").collect();
-                let mut path_found = false;
-                //println!("{:?}", list_paths);
-                for dir in list_paths {
-                    let full_path = Path::new(dir).join(cmd);
-                    if  full_path.is_file() {
-                        println!("{} is {}", cmd, full_path.to_str().unwrap());
-                        path_found = true;
-                        break;
-                    } 
-                }
-                if !path_found {
-                    println!("{}: not found", cmd); // Type not found
-                }
-            } else {
-                println!("{}: not found", cmd); // Type not found
-            }
-        } else {
-            println!("Program was passed {} args (including program name).", args.len());
-            for (i, arg) in args.iter().enumerate() {
-                if i == 0 {
-                    println!("Arg #{} (program name): {}", i, arg);
-                } else {
-                    println!("Arg #{}: {}", i, arg)
-                }
-            }
-        }
-
-    } else {
+    if args.is_empty() {
         println!("{}: command not found", input.trim()); // Default all commands invalid
+        return;
+    }
+
+    match args[0] {
+        // Builtin `exit`
+        "exit" if args.get(1).map_or(false, |&arg| arg == "0") => std::process::exit(0),
+        // Builtin `echo`
+        "echo" => {
+            println!("{}", args[1..].join(" "));
+        },
+        // Builtin `type` builtins
+        "type" if args.len() > 1 => handle_type_command(&args[1..]),
+        args_ => {
+            let (cmd, args) = if args_.contains(" ") {
+                args_.split_once(" ").unwrap()
+            } else {
+                (args_, "")
+            };
+
+            if let Some(path) = env::var_os("PATH") {
+                for dir in env::split_paths(&path) {
+                    let full_path = dir.join(cmd);
+                    if full_path.exists() {
+                        let output = Command::new(cmd).args(args.split(" ")).output().unwrap();
+                        print!("{}", String::from_utf8_lossy(&output.stdout));
+                    }
+                    else {
+                        println!("{}: command not fonud", cmd);
+                    }
+                }
+            }
+        },
     }
 }
 
